473bc0b85d12aa908ba9c398d0406a393beb6abb
Yannick Carissan <ycarissan@gmail.com>
2023-06-27 18:45:01 +0200

    [YC] 2 panels!

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 import sys
 import rdkit.Chem
 import rdkit.Chem.rdMolAlign
+import geometry.geometry
+import graph_theory.detect_cycle
+import numpy as np
+import pandas as pd
+import plotter.plotter
 
-refname = sys.argv[1]
-prbname = sys.argv[2]
-a0 = int(sys.argv[3])
-b0 = int(sys.argv[4])
-c0 = int(sys.argv[5])
-a1 = int(sys.argv[6])
-b1 = int(sys.argv[7])
-c1 = int(sys.argv[8])
-refMol = rdkit.Chem.MolFromXYZFile(refname)
-prbMol = rdkit.Chem.MolFromXYZFile(prbname)
-rmsd = rdkit.Chem.rdMolAlign.AlignMol(prbMol, refMol, atomMap=[(a0,a1), (b0,b1), (c0,c1)])
-rdkit.Chem.MolToXYZFile(refMol, "reference.xyz")
-rdkit.Chem.MolToXYZFile(prbMol, "probe.xyz")
-print(rmsd)
+def main(refname, prbname, a0, b0, c0, a1, b1, c1):
+    refMol = rdkit.Chem.MolFromXYZFile(refname)
+    prbMol = rdkit.Chem.MolFromXYZFile(prbname)
+    jmol_colors_df = pd.read_csv("./jmol_colors.csv")
+    print(jmol_colors_df)
+
+    rmsd = rdkit.Chem.rdMolAlign.AlignMol(prbMol, refMol, atomMap=[(a0,a1), (b0,b1), (c0,c1)])
+    rdkit.Chem.MolToXYZFile(refMol, "reference.xyz")
+    rdkit.Chem.MolToXYZFile(prbMol, "probe.xyz")
+    print(rmsd)
+
+    p=plotter.plotter.MyPlotter()
+
+    p.add_ref(refname, jmol_colors_df)
+    p.add_prb(prbname, jmol_colors_df)
+
+    p.pl.link_views()
+    p.pl.show()
+
+if __name__ == "__main__":
+    refname = sys.argv[1]
+    prbname = sys.argv[2]
+    a0 = int(sys.argv[3])
+    b0 = int(sys.argv[4])
+    c0 = int(sys.argv[5])
+    a1 = int(sys.argv[6])
+    b1 = int(sys.argv[7])
+    c1 = int(sys.argv[8])
+    main(refname, prbname, a0, b0, c0, a1, b1, c1)
---
+++ plotter/plotter.py
@@ -0,0 +1,67 @@
+import geometry.geometry
+import graph_theory.detect_cycle
+import numpy as np
+
+try :
+    import pyvista as pv
+except ModuleNotFoundError as error:
+    pyvista = None
+    print("pyvista module not found")
+
+class MyPlotter():
+    def __init__(self):
+        self.pl = pv.Plotter(shape=(1,2))
+
+    def add_ref(self, refname, jmol_colors_df):
+        self.pl.subplot(0, 0)
+        self.pl.add_text("Reference", font_size=30)
+        spheres, colors, cylinders = get_spheres_colors_and_cylinders(refname, jmol_colors_df)
+        for sphere,color in zip(spheres, colors):
+            self.pl.add_mesh(sphere, color=color, show_edges=False)
+        for cyl in cylinders:
+            self.pl.add_mesh(cyl, color="tan", show_edges=False)
+
+    def add_prb(self, prbname, jmol_colors_df):
+        self.pl.subplot(0, 1)
+        self.pl.add_text("Probe", font_size=30)
+        spheres, colors, cylinders = get_spheres_colors_and_cylinders(prbname, jmol_colors_df)
+        for sphere,color in zip(spheres, colors):
+            self.pl.add_mesh(sphere, color=color, show_edges=False)
+        for cyl in cylinders:
+            self.pl.add_mesh(cyl, color="tan", show_edges=False)
+
+def get_spheres_colors_and_cylinders(filename, jmol_colors_df):
+    geom = geometry.geometry.Geometry(filename)
+    spheres = []
+    cylinders = []
+    colors = []
+    for at in geom.atoms:
+        mesh_sphere = pv.Sphere(radius=0.5, center=[at['x'], at['y'], at['z']])
+        a=jmol_colors_df[jmol_colors_df.Element==at["label"]]
+        try :
+            R = a.R.values[0]/255
+            G = a.G.values[0]/255
+            B = a.B.values[0]/255
+        except :
+#            print(a)
+#            print(at)
+            R=0
+            G=255
+            B=0
+        color=[R, G, B]
+        spheres.append(mesh_sphere)
+        colors.append(color)
+    molecularGraph = graph_theory.detect_cycle.MolecularGraph(filename)
+    for e in molecularGraph.getEdges():
+        idx1 = e.GetBeginAtomIdx()
+        idx2 = e.GetEndAtomIdx()
+        at1 = geom.getAtom(idx1)
+        at2 = geom.getAtom(idx2)
+        pos1 = np.asarray([at1['x'], at1['y'], at1['z']])
+        pos2 = np.asarray([at2['x'], at2['y'], at2['z']])
+        vect_bond = pos2 - pos1
+        middle_bond = 0.5 * (pos1 + pos2)
+
+        mesh_cylinder = pv.Cylinder(center=middle_bond, direction=vect_bond, radius=.2, height=np.linalg.norm(vect_bond))
+        cylinders.append(mesh_cylinder)
+    return spheres, colors, cylinders
